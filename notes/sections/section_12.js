71d40b9312373ffec9592b983ae4c647a0d89c42
thelazybusycoder <thelazybusycoder@gmail.com>
2024-09-11 13:35:44 +0530

    'update'

--- notes/sections/section_12.js
+++ notes/sections/section_12.js
@@ -1,4 +1,28 @@
-const data = {};
+const data = {
+  code1: `import 'package:flutter_test/flutter_test.dart';
+import 'package:ibmi/utils/calculator.dart';
+
+void main() {
+  test(
+    'Given height and weight When CalculateBMI button pressed Then correct BMI return',
+    () {
+      //Arrange
+      const double height = 70, weight = 160;
+      //Act
+      double bmi = CalculateBMI(height, weight);
+      //Assert
+      expect(bmi, 20);
+    },
+  );
+}
+`,
+  code2: `import 'dart:math';
+
+double CalculateBMI(double height, double weight) {
+  return 703 * (weight / pow(height, 2));
+}
+`,
+};
 
 function addCode(id) {
   document.getElementById(id).innerHTML = data[id];
